bf4a8b2346e3f52fb9e815f554323a532f369231
Giorge Lucas <giorgelucasx@gmail.com>
2024-08-13 16:11:32 -0300

    Add Lazyvim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,16 @@
+require('windowPaddingRemove')
+
+vim.cmd([[ syntax on ]])
+vim.cmd([[set nu]])
+vim.cmd([[set tabstop=4]])
+vim.cmd([[set softtabstop=4]])
+vim.cmd([[set nu]])
+vim.cmd([[set tabstop=4]])
+vim.cmd([[set softtabstop=4]])
+vim.cmd([[set shiftwidth=4]])
+vim.cmd([[set expandtab]])
+vim.cmd([[set smarttab]])
+vim.cmd([[set smartindent]])
+vim.cmd([[set cmdheight=0]])
+
+
